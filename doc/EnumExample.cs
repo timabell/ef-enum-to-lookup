52bb3c796116d809edb68f3befba394c8270a48f
Tim Abell <tim@timwise.co.uk>
2014-09-28 05:35:00 +0100

    Example usage class added

---
+++ doc/EnumExample.cs
@@ -0,0 +1,38 @@
+﻿using System.Data.Entity;
+using EfEnumToLookup.LookupGenerator;
+using NUnit.Framework;
+
+namespace EfLookupTesting
+{
+	[TestFixture]
+	public class EnumExample
+	{
+		[Test]
+		public void DoStuff()
+		{
+			var enumToLookup = new EnumToLookup();
+			using (var context = new MyDbContext())
+			{
+				enumToLookup.Apply(context);
+			}
+		}
+	}
+
+	public class MyDbContext : DbContext
+	{
+		public DbSet<Foo> Foos { get; set; }
+	}
+
+	public class Foo
+	{
+		public int Id { get; set; }
+		public Size Size { get; set; }
+	}
+
+	public enum Size
+	{
+		Small = 1, //db friendly id
+		Medium,
+		ReallyVeryBig
+	}
+}
