b635e2ec5835453abebb0eec043ed8cb557761d1
Tim Abell <tim@timwise.co.uk>
2014-09-28 05:43:16 +0100

    Code doc on example cs

--- doc/EnumExample.cs
+++ doc/EnumExample.cs
@@ -4,12 +4,22 @@ using NUnit.Framework;
 
 namespace EfLookupTesting
 {
+	/// <summary>
+	/// Example usage of the ef lookup generator.
+	/// To run this create a project, drop this file in,
+	/// add EF 6.1 and NUnit through nuget and run the test.
+	/// Open up (localdb)\v11.0 in Sql Server Object Explorer
+	/// and look inside the new it has generated to see an
+	/// example of the generated Enum table and its content.
+	/// </summary>
 	[TestFixture]
 	public class EnumExample
 	{
 		[Test]
 		public void DoStuff()
 		{
+			// This would normally be run inside either a db initializer seed
+			// or the migration seed method.
 			var enumToLookup = new EnumToLookup();
 			using (var context = new MyDbContext())
 			{
