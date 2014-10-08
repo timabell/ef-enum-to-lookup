7b64ea61c095f72728477764072a62135e550baa
Tim Abell <tim@timwise.co.uk>
2014-10-08 22:13:43 +0100

    Added EnumGeneratorException
    To be used if any of the expectations of the model parser aren't met. Currently just uses .Single calls.

---
+++ EfEnumToLookup/LookupGenerator/EnumGeneratorException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace EfEnumToLookup.LookupGenerator
+{
+    [Serializable]
+    public class EnumGeneratorException : Exception
+    {
+        public EnumGeneratorException() : base() { }
+
+        public EnumGeneratorException(string message): base(message) { }
+
+        public EnumGeneratorException(string message, Exception innerException): base(message, innerException) { }
+
+        public EnumGeneratorException(SerializationInfo serializationInfo, StreamingContext streamingContext) : base(serializationInfo, streamingContext) { }
+    }
+}
