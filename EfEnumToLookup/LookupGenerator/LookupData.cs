422adab8b39e448cff1be1b465b4f0a2b5bc8565
Tim Abell <tim@timwise.co.uk>
2015-03-23 20:03:05 +0000

    Debug display for LookupData
    just so it's easier to see what's in the list when debugging

--- EfEnumToLookup/LookupGenerator/LookupData.cs
+++ EfEnumToLookup/LookupGenerator/LookupData.cs
@@ -1,7 +1,10 @@
+using System.Diagnostics;
+
 namespace EfEnumToLookup.LookupGenerator
 {
 	using System.Collections.Generic;
 
+	[DebuggerDisplay("LookupData {Name}")]
 	internal class LookupData
 	{
 		public string Name { get; set; }
