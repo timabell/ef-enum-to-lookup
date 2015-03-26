7df2f2e040e7af611d88315ee953913a52b5994e
Tim Abell <tim@timwise.co.uk>
2015-03-26 15:35:13 +0000

    added unit tests for enum parser

---
+++ EFTests/Tests/EnumParserTests.cs
@@ -0,0 +1,65 @@
+﻿using System.Linq;
+using EfEnumToLookup.LookupGenerator;
+using NUnit.Framework;
+
+namespace EFTests.Tests
+{
+	[TestFixture]
+	public class EnumParserTests
+	{
+		[Test]
+		public void ReadsName()
+		{
+			// arrange
+			var parser = new EnumParser { SplitWords = false };
+
+			// act
+			var result = parser.GetLookupValues(typeof(BareEnum));
+
+			// assert
+			Assert.AreEqual("FooBar", result.Single().Name);
+		}
+
+		[Test]
+		public void ReadsSplitName()
+		{
+			// arrange
+			var parser = new EnumParser { SplitWords = true };
+
+			// act
+			var result = parser.GetLookupValues(typeof(BareEnum));
+
+			// assert
+			Assert.AreEqual("Foo Bar", result.Single().Name);
+		}
+
+
+		[Test]
+		public void ReadsDecoratedName()
+		{
+			// arrange
+			var parser = new EnumParser { SplitWords = true };
+
+			// act
+			var result = parser.GetLookupValues(typeof(DecoratedEnum));
+
+			// assert
+			Assert.AreEqual("Wide boy", result.Single().Name);
+		}
+
+		private enum BareEnum
+		{
+			// ReSharper disable once UnusedMember.Local
+			// used by test suite
+			FooBar
+		}
+
+		private enum DecoratedEnum
+		{
+			// ReSharper disable once UnusedMember.Local
+			// used by test suite
+			[System.ComponentModel.Description("Wide boy")]
+			FooBar
+		}
+	}
+}
